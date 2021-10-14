91d809bc1b75a8b63fdfd275f52baf1fa12537c2
min-son <89447370+min-son@users.noreply.github.com>
2021-10-14 17:28:53 +0900

    Add files via upload

---
+++ 3_2_file.py
@@ -0,0 +1,34 @@
+students = ["egoing", "sori", "maru"]
+grades = [2, 1, 4]
+print(students[1])
+print(len(students))
+print(max(grades))
+
+import statistics  
+print(statistics.mean(grades))   #평균값   
+
+import random
+print(random.choice(students))
+
+print('hello world')
+print("hello world")
+print('''
+hello
+world
+''')
+
+print('1'+'1')
+
+print(-1)
+print(0)
+print(1) # int
+print(1.1) # float
+print(1+1)
+print(1-2)
+
+import math
+print(math.sqrt(4))
+
+import random
+print(random.random(1,7))
+
