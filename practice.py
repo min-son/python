78688301a25c16dd796600b52ad7afdad557572f
min-son <89447370+min-son@users.noreply.github.com>
2021-10-21 17:25:13 +0900

    Add files via upload

---
+++ practice.py
@@ -0,0 +1,40 @@
+# print(5)
+# print(-10)
+# print(3.14)
+# print(1000)
+# print(5+3)
+# print('풍선')
+# print('ㅋ'*9)
+
+
+# #bulean 참/거짓
+# print(5>10)
+# print(5<10)
+# print(True)
+
+
+#변수
+#애완동물을 소개해주세요
+
+# animal = "고양이"
+# name = "해피"
+# age = 4
+# hobby = "낮잠"
+# is_adult = age >=3
+
+# '''여러문장을
+# 주석처리 
+# 해버리기!'''
+
+
+# print("우리집 "+ animal + "의 이름은 "+ name + "에요")
+# hobby = "공놀이" #마지막에 선언된 변수가 적용됨
+# #print(name + "는 "+ str(age) + "이며, "+ hobby + "을 아주 좋아해요")  #정수형 출력시 str로 표현
+# print(name , "는 ", str(age) , "이며, ", hobby , "을 아주 좋아해요")
+# print(name + "는 어른일까요? " + str(is_adult))
+
+from random import *
+
+print(random())
+print(random() * 10)
+print(int(random() * 10))
